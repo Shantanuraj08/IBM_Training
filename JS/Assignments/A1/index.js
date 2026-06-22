4aaedfbd4d06fb2d7798e3874ef342b66d364d6d
Shantanuraj08 <rajsantanu08@gmail.com>
2026-06-22 14:55:04 +0530

    Add HTML, CSS, and JavaScript files for various assignments and interview questions

---
+++ JS/Assignments/A1/index.js
@@ -0,0 +1,73 @@
+// Q1
+{
+let A = 2;
+let B = 5;
+
+let result = A + B;
+
+console.log(result);
+}
+// Q2
+{
+
+
+let A = 5;
+let B = 3;
+
+if (A < 10 && A > B) {
+    console.log(true);
+} else {
+    console.log(false);
+}
+
+}
+// q3
+{
+
+let A = 20;
+let B = 7;
+
+if (A % 10 === 0 || B % 10 === 0) {
+    console.log(true);
+} else {
+    console.log(false);
+}
+}
+// Q4
+{
+let N = 4567;
+
+console.log(Math.floor(N / 1000));
+}
+
+// Q5
+{
+let N = 4567;
+
+console.log(N % 10);
+}
+// Q6
+{
+let A = 2; // divisor
+let B = 9; // dividend
+
+console.log(B % A);
+}
+// Q7
+{
+let A = 2;
+let B = 5;
+
+console.log(A * B);
+}
+// Q8
+{
+let A = 50;
+let B = 20;
+let C = 100;
+
+let sum = A + B + C;
+let average = sum / 3;
+
+console.log(sum, average.toFixed(2));
+}
---
+++ JS/Day1/index.js
@@ -0,0 +1,12 @@
+var a=10;
+a=50;
+console.log(a);
+
+let b =12;
+b=13;
+// let b=23; redeclertion is not possible for let
+console.log(b);
+
+const c=15;
+// c=14; reassignment is not possible for const
+console.log(c);
