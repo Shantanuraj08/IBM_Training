eee19a838fe5de2d2a14888035ac0025859cced6
Shantanuraj08 <rajsantanu08@gmail.com>
2026-06-25 09:07:03 +0530

    Add functions and arrow functions examples with console logs

--- JS/Day3/index.js
+++ JS/Day3/index.js
@@ -19,3 +19,56 @@ console.log(arr3.findIndex(x=>x==2))
 const arr4=[1,2,3,4,5,6]
 console.log(arr3.splice(1,3))
 
+function func(name,age){
+    return console.log("Regualar function",name,age);
+}
+func("Raj",22)
+
+let a= function (name,age){
+      console.log(name,age)
+}
+a("Raj",22)
+
+const foo = (name, age) => {
+    console.log(name, age);
+}
+
+foo("raj",22)
+
+
+
+function greet(){
+    console.log("Hello world")
+}
+greet();
+
+let ar = ()=>{
+    console.log("arrow functin with no return statment")
+}
+
+ar(); 
+
+let det= function (firstname,lastname,age ){
+      return console.log(`${firstname ,lastname} is ${age} year old `)
+}
+
+det(Shantanu,Raj,22);
+
+
+[
+  {
+    "id": 1,
+    "name": "Shantanu",
+    "age": 24
+  },
+  {
+    "id": 2,
+    "name": "Rahul",
+    "age": 22
+  },
+  {
+    "id": 3,
+    "name": "Priya",
+    "age": 23
+  }
+]
